1b3a41c55ece5ffb6aabf1eccc8e5df22d2a853e
Avengerians <101318784+Avengerians@users.noreply.github.com>
2022-03-11 17:59:48 +0530

    Update dev.py

--- Avenger/modules/dev.py
+++ Avenger/modules/dev.py
@@ -22,9 +22,9 @@ def allow_groups(update: Update, context: CallbackContext):
         update.effective_message.reply_text(f"Current state: {state}")
         return
     if args[0].lower() in ["off", "no"]:
-        Executive.ALLOW_CHATS = True
+        Avenger.ALLOW_CHATS = True
     elif args[0].lower() in ["yes", "on"]:
-        Executive.ALLOW_CHATS = False
+        Avenger.ALLOW_CHATS = False
     else:
         update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
         return
