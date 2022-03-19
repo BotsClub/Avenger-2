d33f70d91f187c3590cc5dcb975579ee6353af25
Avengerians <101318784+Avengerians@users.noreply.github.com>
2022-03-19 22:59:39 +0530

    Update __init__.py

--- Avenger/modules/sql/__init__.py
+++ Avenger/modules/sql/__init__.py
@@ -1,15 +1,25 @@
-from Avenger import DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
+from Avenger import DB_URL, LOGGER as log
+
+if DB_URL and DB_URL.startswith("postgres://"):
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
 
 def start() -> scoped_session:
     engine = create_engine(DB_URL, client_encoding="utf8")
+    log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=True))
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION = start()
+except Exception as e:
+    log.exception(f'[PostgreSQL] Failed to connect due to {e}')
+    exit()
+   
+log.info("[PostgreSQL] Connection successful, session started.")
