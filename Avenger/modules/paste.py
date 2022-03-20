df83d6d86c1e93b6365b2cf0a37af30d94afacad
Avengerians <101318784+Avengerians@users.noreply.github.com>
2022-03-20 18:44:41 +0530

    Update paste.py

--- Avenger/modules/paste.py
+++ Avenger/modules/paste.py
@@ -1,64 +1,24 @@
-import asyncio
-import os
-import re
-
-import aiofiles
-from pykeyboard import InlineKeyboard
+from requests import post, get
+from Avenger import pbot as bot
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton
-
-from Avenger import pbot, aiohttpsession
-from Avenger.utils.errors import capture_err
-from Avenger.utils.pastebin import paste
-
-pattern = re.compile(r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$")
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-async def isPreviewUp(preview: str) -> bool:
-    for _ in range(7):
-        try:
-            async with aiohttpsession.head(preview, timeout=2) as resp:
-                status = resp.status
-                size = resp.content_length
-        except asyncio.exceptions.TimeoutError:
-            return False
-        if status == 404 or (status == 200 and size == 0):
-            await asyncio.sleep(0.4)
-        else:
-            return status == 200
-    return False
+def paste(text):
+    url = "https://spaceb.in/api/v1/documents/"
+    res = post(url, data={"content": text, "extension": "txt"})
+    return f"https://spaceb.in/{res.json()['payload']['id']}"
 
 
-@pbot.on_message(filters.command("paste") & ~filters.edited)
-@capture_err
-async def paste_func(_, message):
-    if not message.reply_to_message:
-        return await message.reply_text("Reply To A Message With /paste")
-    m = await message.reply_text("Pasting...")
-    if message.reply_to_message.text:
-        content = str(message.reply_to_message.text)
-    elif message.reply_to_message.document:
-        document = message.reply_to_message.document
-        if document.file_size > 1048576:
-            return await m.edit("You can only paste files smaller than 1MB.")
-        if not pattern.search(document.mime_type):
-            return await m.edit("Only text files can be pasted.")
-        doc = await message.reply_to_message.download()
-        async with aiofiles.open(doc, mode="r") as f:
-            content = await f.read()
-        os.remove(doc)
-    link = await paste(content)
-    preview = link + "/preview.png"
-    button = InlineKeyboard(row_width=1)
-    button.add(InlineKeyboardButton(text="Paste Link", url=link))
+@bot.on_message(filters.command('paste'))
+def pastex(_, message):
+    text = message.reply_to_message
+    if text:
+        x = paste(text.text)
+        message.reply(x,
+                      reply_markup=InlineKeyboardMarkup(
+                          [[InlineKeyboardButton("Open", url=x)]]),
+                      disable_web_page_preview=True)
 
-    if await isPreviewUp(preview):
-        try:
-            await message.reply_photo(photo=preview, quote=False, reply_markup=button)
-            return await m.delete()
-        except Exception:
-            pass
-    return await m.edit(link)
-
-
-__mod_name__ = "Paste"
+    else:
+        message.reply_text("Reply to a message!")
