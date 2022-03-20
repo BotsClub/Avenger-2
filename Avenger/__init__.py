84aefb558fb78f6e251edc593e57a4b10212bf2d
Avengerians <101318784+Avengerians@users.noreply.github.com>
2022-03-20 22:18:07 +0530

    Update __init__.py

--- Avenger/__init__.py
+++ Avenger/__init__.py
@@ -102,6 +102,7 @@ if ENV:
     DB_URL = os.environ.get("DATABASE_URL")
     DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
     FDB_URL = os.environ.get("FSUBDATABASE_URL")
+    NDB_URL = os.environ.get("NMODEDATABASE_URL")
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     ARQ_API = os.environ.get("ARQ_API", None)
