b1c31418aec863693afddd128287321943f71ba1
Avengerians <101318784+Avengerians@users.noreply.github.com>
2022-03-27 21:53:01 +0530

    Update userinfo.py

--- Avenger/modules/userinfo.py
+++ Avenger/modules/userinfo.py
@@ -461,11 +461,11 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>▬▬▬「 ᴀᴠᴇɴɢᴇʀ ꜱᴛᴀᴛꜱ 」▬▬▬</b>\n" + "\n".join(
+    stats = "<b>▬▬「ᴀᴠᴇɴɢᴇʀ ꜱᴛᴀᴛꜱ」▬▬</b>\n" + "\n".join(
         [mod.__stats__() for mod in STATS]
     )
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    result += "\n<b>▬▬▬「 ᴀᴠᴇɴɢᴇʀ ꜱᴛᴀᴛꜱ 」▬▬▬</b>"
+    result += "\n<b>▬▬「ᴀᴠᴇɴɢᴇʀ ꜱᴛᴀᴛꜱ」▬▬</b>\n       ▬▬▬▬▬▬▬▬\n       ▬▬▬▬▬▬▬▬"
     update.effective_message.reply_text(
         result,
         parse_mode=ParseMode.HTML,
