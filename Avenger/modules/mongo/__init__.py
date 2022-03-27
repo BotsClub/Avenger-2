17c53dfc36a5f3e6f9529347f2c1a3d847ed0630
Avengerians <101318784+Avengerians@users.noreply.github.com>
2022-03-27 08:59:28 +0530

    Delete __init__.py

--- Avenger/modules/mongo/__init__.py
+++
@@ -1,7 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient as AvengerMongoClient
-from Avenger import MONGO_DB_URI
-
-avengermongo = AvengerMongoClient(MONGO_DB_URI)
-avengerdb = avengermongo.avenger
-
-anitcdb = avengerdb.antichnl
